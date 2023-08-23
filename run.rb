00bda392168e3994d62c50b96f85e5f24db6c6e0
Ebanyyy <vaniegabu@gmail.com>
2023-08-23 10:05:21 +0800

    comitted

--- run.rb
+++ run.rb
@@ -11,6 +11,10 @@ station1 = Station.new("Tamparuli", 10, 20)
 station2 = Station.new("Kota Belud", 30, 40)
 station3 = Station.new("Kota Kinabalu", 40, 50)
 
+puts "Welcome to DBKK Transportation"
+puts "#{rider1.name}"
+puts "#{rider1.id_number}"
+
 
 card = Card.new("student", "12345")
 puts card.card_type
