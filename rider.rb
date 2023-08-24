43c81db35b8b975691f78378ccc8dbcff79290df
Ebanyyy <vaniegabu@gmail.com>
2023-08-24 16:18:28 +0800

    Commit

--- rider.rb
+++ rider.rb
@@ -1,14 +1,12 @@
 class Rider
-	attr_reader :name, :contact_number, :id_number
+	attr_reader :name, :contact_number, :id_number, :card
 
 	def initialize(name, contact_number, id_number)
+
 		@name = name 
 		@contact_number = contact_number
 		@id_number = id_number
+		@card = Card.new("Student")
 	end
 end
 
-rider = Rider.new("Bany", "014-1231234", "123")
-puts rider.name 
-puts rider.contact_number 
-puts rider.id_number
